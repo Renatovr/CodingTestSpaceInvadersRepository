b750929ef5e4660f604b6b1b4c39b9635f98ee00
Renatovr <renato.engvr@gmail.com>
2022-05-21 09:58:09 +0100

    player movement implementation

---
+++ CodingTest SpaceInvaders/Assets/Scripts/Player/Controllers/PlayerMovementController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceInvaders.Player
+{
+    /// <summary>
+    /// Class which will handle the logical part of the player's movement
+    /// </summary>
+    public class PlayerMovementController
+    {
+        private const string MOVEMENT_AXIS_NAME = "Horizontal";
+
+        private float m_MovementSpeed;
+
+        /// <summary>
+        /// Construct a new instance of PlayerMovement controller, providing the relevant data.
+        /// </summary>
+        /// <param name="movementSpeed">Speed with which the object will calculate movement.</param>
+        public PlayerMovementController (float movementSpeed)
+        {
+            m_MovementSpeed = movementSpeed;
+        }
+
+        /// <summary>
+        /// Calculate value for a new position after movement is done.
+        /// </summary>
+        /// <param name="currentPosition">The position before movement calculation.</param>
+        /// <returns>Returns an updated position after movement is calculated from currentPosition.</returns>
+        public Vector3 GetUpdatedPosition (Vector3 currentPosition)
+        {
+            var movementInput = Input.GetAxisRaw(MOVEMENT_AXIS_NAME);
+
+            var newPosition = currentPosition;
+            newPosition.x += movementInput * m_MovementSpeed * Time.deltaTime;
+            newPosition.x = GetClampedToCameraHorizontalView(newPosition.x);
+
+            return newPosition;
+        }
+
+        private float GetClampedToCameraHorizontalView (float currentPosition)
+        {
+            var clampedPosition = currentPosition;
+            var camera = Camera.main;
+
+            if(camera != null)
+            {
+                //Get world positions for the left and right borders of the camera viewport
+                var leftBorder = camera.ViewportToWorldPoint(Vector3.zero);
+                var rightBorder = camera.ViewportToWorldPoint(Vector3.right);
+                clampedPosition = Mathf.Clamp(clampedPosition, leftBorder.x, rightBorder.x);
+            }
+
+            return clampedPosition;
+        }
+    }
+}
---
+++ CodingTest SpaceInvaders/Assets/Scripts/Player/PlayerView.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace SpaceInvaders.Player
+{
+    /// <summary>
+    /// The class to hold and handle the general behaviour of the player ship.
+    /// </summary>
+    public class PlayerView : MonoBehaviour
+    {
+        [Tooltip("The speed with which the player will move horizontally, while being controlled.\n" +
+            "This value will be applied on initialization, and will not be updated in real-time.")]
+        [SerializeField] private float m_MovementSpeed = 5f;
+
+        private PlayerMovementController m_MovementController;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Init();
+        }
+
+        private void Init ()
+        {
+            m_MovementController = new PlayerMovementController(m_MovementSpeed);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position = m_MovementController.GetUpdatedPosition(transform.position);
+        }
+    }
+}
